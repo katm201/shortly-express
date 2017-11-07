f81b7ee151af9eddd28b5b46f00d4653dc3a301b
Hack Reactor Student <student@hackreactor.com>
2017-11-06 17:39:31 -0800

    Fix signup loop to redirect back to signup if username already exists--passing first 8 tests

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,7 @@
 const parseCookies = (req, res, next) => {
+  //access the cookies on request
+  //parse them into an object
+  //assign this object to a cookies property request
 };
 
 module.exports = parseCookies;
